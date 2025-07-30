6d5bee7b0b20b8992f005113828f863812250c79
Mothilal Jadhav <mothilaljadhav@Mothilals-MacBook-Air-7.local>
2025-07-30 10:12:13 +0530

    created a server for chatting

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+# Create a TCP/IP socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the server
+client.connect(('127.0.0.1', 55555))
+
+# Ask for username
+nickname = input("Choose your nickname: ")
+
+# Function to receive messages from the server
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+# Function to write messages to the server
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('utf-8'))
+
+# Start threads for send and receive
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
