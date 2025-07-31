ed8fc8d671f4e3210a8f90b417aa72ff292df19b
Mothilal Jadhav <mothilaljadhav@Mothilals-MacBook-Air-10.local>
2025-07-31 12:08:32 +0530

    modified client.py

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+from colorama import Fore, Style
+import datetime
 
 # Create a TCP/IP socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,14 +12,30 @@ client.connect(('127.0.0.1', 55556))
 # Ask for username
 nickname = input("Choose your nickname: ")
 
+log_file = "chat_history.txt"
+
+def log_message(msg):
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open(log_file, "a") as f:
+        f.write(f"[{timestamp}] {msg}\n")
+
 # Function to receive messages from the server
 def receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            print(message)
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                if message.startswith(f"{nickname}:"):
+                    # Print your own message in green
+                    print(Fore.GREEN + message + Style.RESET_ALL)
+                else:
+                    # Print others' messages normally
+                    print(message)
+                log_message(message)
         except:
-            print("An error occurred!")
+            print("Error! Connection to server lost.")
             client.close()
             break
 
@@ -26,6 +44,8 @@ def write():
     while True:
         message = f'{nickname}: {input("")}'
         client.send(message.encode('utf-8'))
+
+
 
 # Start threads for send and receive
 receive_thread = threading.Thread(target=receive)
